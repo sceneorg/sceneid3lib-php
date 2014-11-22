5b126fd74eb3e07e929bd53ac17291165ff9cceb
Gargaj <gargaj@conspiracy.hu>
2014-11-22 15:30:58 +0100

    fix a few things

--- example.php
+++ example.php
@@ -21,19 +21,18 @@ $sceneID = new SceneID3( array(
 try
 {
   $sceneID->GetClientCredentialsToken();
+
+  // We got it, now we can query at will
+  $result = $sceneID->User(1);
+  print_r($result);
 }
 catch( SceneID3AuthException $e )
 {
   // something went wrong
 }
 
-// We got it, now we can query at will
-
-$result = $sceneID->User(1);
-print_r($result);
-
 /////////////////////////////////////////////////
-// Act 2 - Operations that don't require a user
+// Act 2 - Operations that do require a user
 //
 // This is a bit trickier
 // First, we redirect to the SceneID login page
@@ -49,7 +48,6 @@ try
   // Note: this only needs to be done once, the tokens are stored in sessions
 
   // We can now identify the user by their login
-  
   $result = $sceneID->Me();
   print_r($result);
 }
